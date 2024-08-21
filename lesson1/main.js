0dbcbd7b3f9c14ac4b09dd22ebf609d12f8b38b8
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-08-22 02:41:09 +0300

    lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,32 @@
+let num = 100500; //number
+console.log(num);
+
+let s = "hello okten";
+s = "js is cool"; //перевизначення змінної
+s = "12323232"; //цифри але не цифри, бо тепер рядок)
+console.log(s); //string
+
+let b = true; //булевий вираз > < >= <= == != === !==
+console.log(b);
+b = false;
+console.log(b);
+
+let bResult = 5 > 6; //false
+console.log(bResult);
+
+let a = 10;
+let c = 5;
+
+// + - / * %, де % - ділення по модулю (тобто показує чи є остача чи ні після ділення)
+
+console.log(a % c);
+
+console.log("asd" + "qge"); // конкатенація ( конкат це з'єднання )
+
+let s1 = "okten";
+
+let s2 = "hello";
+
+let space = " ";
+
+console.log(s1 + space + s2);
