b626cb660f19c446f12244bb4d0527f97240b857
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-08-25 16:46:18 +0300

    hw

---
+++ hw1/main.js
@@ -0,0 +1,54 @@
+// let first = "hello";
+// let second = "owu";
+// let third = "com";
+// let fourth = "ua";
+// let five = 1;
+// let six = 10;
+// let seven = -999;
+// let eight = 123;
+// let nine = 3.14;
+// let ten = 2.7;
+// let eleven = 16;
+// let twelve = true;
+// let thirteen = false;
+
+// console.log(first);
+
+// console.log(second);
+
+// console.log(third);
+
+// console.log(fourth);
+
+// console.log(five);
+
+// console.log(six);
+
+// console.log(seven);
+
+// console.log(eight);
+
+// console.log(nine);
+
+// console.log(ten);
+
+// console.log(eleven);
+
+// console.log(twelve);
+
+// console.log(thirteen);
+
+// let firstName = "Ihor";
+// let middleName = "Pavlovych";
+// let lastName = "Haraniuk";
+// let space = " ";
+// let person = firstName + space + middleName + space + lastName;
+// console.log(person);
+
+let a = 100;
+let b = "100";
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
